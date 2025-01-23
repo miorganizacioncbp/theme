784f72ce21e97b131cfab610e9ec7c456f655843
Camilabperez <Camilabperez@hotmail.com>
2025-01-23 10:58:34 -0300

    responsive staff

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -40,19 +40,19 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong> Mensaje enviado </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
 
-        error: function() {
+      error: function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Perdon, " + firstName + ", no es posible enviar tu mensaje. Por favor intenta más tarde."));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
